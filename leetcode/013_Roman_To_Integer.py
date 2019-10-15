f6ca9a87c6cc0e60c2d1ef89128b83fb63b63663
musedy <charles09015@gmail.com>
2019-10-15 20:57:44 +0800

    first commit

---
+++ leetcode/013_Roman_To_Integer.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        digits = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        sum = digits[s[-1]]
+        for i in range(len(s)-2, -1, -1):
+            if digits[s[i]] < digits[s[i+1]]:
+                sum -= digits[s[i]]
+            else:
+                sum += digits[s[i]]
+        return sum
