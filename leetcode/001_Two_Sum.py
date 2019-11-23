7a33b026ed913a40b193ebd648d6b7f2e2938d0c
musedy <charles09015@gmail.com>
2019-11-24 01:20:44 +0800

    rewrote for simplicity

--- leetcode/001_Two_Sum.py
+++ leetcode/001_Two_Sum.py
@@ -5,15 +5,10 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        nums_index = [(v, index) for index, v in enumerate(nums)]
-        nums_index.sort()
-        begin, end = 0, len(nums) - 1
-        while begin < end:
-            curr = nums_index[begin][0] + nums_index[end][0]
-            if curr == target:
-                return [nums_index[begin][1], nums_index[end][1]]
-            elif curr < target:
-                begin += 1
-            else:
-                end -= 1
+        store = {}
+        for i , num in enumerate(nums):
+            if target - num in store:
+                return [store[target - num], i]
+            store[num] = i
+        return []
                 
